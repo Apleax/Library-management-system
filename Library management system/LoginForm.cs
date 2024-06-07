ca9cc475350589f68ade14517e10e3b6b25751ef
Apleax <3220923564@qq.com>
2024-06-07 14:56:24 +0800

    登录界面

--- Library management system/LoginForm.cs
+++ Library management system/LoginForm.cs
@@ -1,5 +1,6 @@
-using Library_management_system.UserForm; using System.Data.SqlClient;
+using System.Data.SqlClient;
 using static Library_management_system.Login_Sign_in;
+
 namespace Library_management_system {     public partial class LoginForm : Form     {         public LoginForm()         {             InitializeComponent();             this.StartPosition = FormStartPosition.CenterScreen;         }
         private void Text_changed(object sender, EventArgs e)
         {
@@ -14,8 +15,39 @@ namespace Library_management_system {     public partial class LoginForm : F
                 }
             }
         }
-        private void LoginButton_Click(object sender, EventArgs e)         {             if (UserName.Text != "" && PassWord.Text != "")
-            {             }             else             {                 Label l = new Label();                 l.Text = "请输入完整用户名或密码";                 l.Location = new Point(255, 250);                 l.AutoSize = true;                 l.Name = "ErrorLabel";                 l.ForeColor = Color.Red;                 this.Controls.Add(l);             }         }
+        private async void LoginButton_Click(object sender, EventArgs e)         {             if (UserName.Text != "" && PassWord.Text != "")
+            {                 if (await Pingtest())
+                {
+                    try
+                    {
+                        using (SqlConnection con = new SqlConnection($"Data Source={DecryptString(GetJsonObject().DataSource)};Initial Catalog={DecryptString(GetJsonObject().InitialCatalog)};User ID={UserName.Text}; Password={MD5encipher(PassWord.Text)}"))
+                        {
+                            con.Open();
+                            UserForm.UserForm userForm = new UserForm.UserForm();
+                            userForm.Show();
+                            this.Hide();
+                            userForm.FormClosing += (s, args) =>
+                            {
+                                this.Show();
+                            };
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        if (ex.Number == 18456)
+                        {
+                            MessageBox.Show("用户名或密码错误", "登录失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("无法连接至远程服务器", "连接错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }             }             else             {                 Label l = new Label();                 l.Text = "请输入完整用户名或密码";                 l.Location = new Point(255, 250);                 l.AutoSize = true;                 l.Name = "ErrorLabel";                 l.ForeColor = Color.Red;                 this.Controls.Add(l);             }         }
         private async void Sign_inButton_Click(object sender, EventArgs e)
         {
             if (UserName.Text != "" && PassWord.Text != "")
