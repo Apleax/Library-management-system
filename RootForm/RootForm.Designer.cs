64ba2ecc331e03f98a67395e7f04964d072bebdd
Apleax <3220923564@qq.com>
2024-06-10 02:32:09 +0800

    管理员界面

---
+++ RootForm/RootForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Library_management_system.AdminForm
+{
+    partial class RootForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Status = new StatusStrip();
+            Timenow = new ToolStripStatusLabel();
+            Result = new ListView();
+            Bookname = new ColumnHeader();
+            Author = new ColumnHeader();
+            Booketype = new ColumnHeader();
+            Booknum = new ColumnHeader();
+            Userstatus = new Label();
+            SelectButton = new Button();
+            UpPage = new Button();
+            DownPage = new Button();
+            Status.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Status
+            // 
+            Status.Font = new Font("Microsoft YaHei UI", 12F);
+            Status.ImageScalingSize = new Size(24, 24);
+            Status.Items.AddRange(new ToolStripItem[] { Timenow });
+            Status.Location = new Point(0, 706);
+            Status.Name = "Status";
+            Status.RightToLeft = RightToLeft.Yes;
+            Status.Size = new Size(1166, 22);
+            Status.SizingGrip = false;
+            Status.TabIndex = 1;
+            // 
+            // Timenow
+            // 
+            Timenow.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            Timenow.Name = "Timenow";
+            Timenow.Size = new Size(0, 15);
+            // 
+            // Result
+            // 
+            Result.Columns.AddRange(new ColumnHeader[] { Bookname, Author, Booketype, Booknum });
+            Result.Font = new Font("Microsoft YaHei UI", 14F);
+            Result.Location = new Point(12, 12);
+            Result.Name = "Result";
+            Result.Scrollable = false;
+            Result.Size = new Size(984, 605);
+            Result.TabIndex = 2;
+            Result.UseCompatibleStateImageBehavior = false;
+            Result.View = View.Details;
+            // 
+            // Bookname
+            // 
+            Bookname.Text = "书名";
+            Bookname.Width = 354;
+            // 
+            // Author
+            // 
+            Author.Text = "作者";
+            Author.Width = 250;
+            // 
+            // Booketype
+            // 
+            Booketype.Text = "类型";
+            Booketype.Width = 200;
+            // 
+            // Booknum
+            // 
+            Booknum.Text = "库存";
+            Booknum.Width = 180;
+            // 
+            // Userstatus
+            // 
+            Userstatus.Font = new Font("Microsoft YaHei UI", 9F);
+            Userstatus.Location = new Point(1002, 12);
+            Userstatus.Name = "Userstatus";
+            Userstatus.Size = new Size(152, 61);
+            Userstatus.TabIndex = 4;
+            Userstatus.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // SelectButton
+            // 
+            SelectButton.Font = new Font("Microsoft YaHei UI", 12F);
+            SelectButton.Location = new Point(1002, 76);
+            SelectButton.Name = "SelectButton";
+            SelectButton.Size = new Size(152, 68);
+            SelectButton.TabIndex = 5;
+            SelectButton.Text = "搜索";
+            SelectButton.UseVisualStyleBackColor = true;
+            SelectButton.Click += SelectButton_Click;
+            // 
+            // UpPage
+            // 
+            UpPage.Font = new Font("Microsoft Sans Serif", 16F);
+            UpPage.Location = new Point(308, 623);
+            UpPage.Name = "UpPage";
+            UpPage.Size = new Size(58, 55);
+            UpPage.TabIndex = 6;
+            UpPage.Text = "<";
+            UpPage.UseVisualStyleBackColor = true;
+            // 
+            // DownPage
+            // 
+            DownPage.Font = new Font("Microsoft Sans Serif", 16F);
+            DownPage.Location = new Point(621, 623);
+            DownPage.Name = "DownPage";
+            DownPage.Size = new Size(58, 55);
+            DownPage.TabIndex = 7;
+            DownPage.Text = ">";
+            DownPage.UseVisualStyleBackColor = true;
+            // 
+            // RootForm
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1166, 728);
+            Controls.Add(DownPage);
+            Controls.Add(UpPage);
+            Controls.Add(SelectButton);
+            Controls.Add(Userstatus);
+            Controls.Add(Result);
+            Controls.Add(Status);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "RootForm";
+            Text = "后台管理";
+            Load += AdminForm_Load;
+            Status.ResumeLayout(false);
+            Status.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private StatusStrip Status;
+        private ToolStripStatusLabel Timenow;
+        private ListView Result;
+        private ColumnHeader Bookname;
+        private ColumnHeader Author;
+        private ColumnHeader Booketype;
+        private ColumnHeader Booknum;
+        private Label Userstatus;
+        private Button SelectButton;
+        private Button UpPage;
+        private Button DownPage;
+    }
+}
---
+++ RootForm/RootForm.cs
@@ -0,0 +1,77 @@
+﻿using System.Data.SqlClient;
+using static Library_management_system.Login_Sign_in;
+using static System.ComponentModel.Design.ObjectSelectorEditor;
+using Timer = System.Windows.Forms.Timer;
+
+namespace Library_management_system.AdminForm
+{
+    public partial class RootForm : Form
+    {
+        string sql;
+        public RootForm(string sql)
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.sql = sql;
+        }
+        private async void AdminForm_Load(object sender, EventArgs e)
+        {
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+            using (SqlConnection con = new SqlConnection(sql))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT SUSER_SNAME()", con);
+                Userstatus.Text = "当前用户：" + cmd.ExecuteScalar().ToString();
+            }
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            this.Timenow.Text = now.ToString("HH:mm");
+        }
+
+        private async void SelectButton_Click(object sender, EventArgs e)
+        {
+            if (await Pingtest())
+            {
+                try
+                {
+                    using (SqlConnection con = new SqlConnection(sql))
+                    {
+                        con.Open();
+                        SqlCommand cmd = new SqlCommand("USE [Library management system];SELECT * FROM Book", con);
+                        SqlDataReader sqlData = cmd.ExecuteReader();
+                        Result.Items.Clear();
+                        while (sqlData.Read())
+                        {
+                            List<string> book = new List<string>();
+                            for (int i = 0; i < sqlData.FieldCount; i++)
+                            {
+                                book.Add(sqlData.GetValue(i).ToString());
+                            }
+                            Result.Items.Add(new ListViewItem(book.ToArray()));
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    if (ex.Number == 18456)
+                    {
+                        MessageBox.Show("用户名或密码错误", "登录失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("无法连接至远程服务器", "连接错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
