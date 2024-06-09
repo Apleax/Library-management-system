a4ba7228d071b658579b255ae8d31c72b7ed0e95
Apleax <3220923564@qq.com>
2024-06-10 02:32:32 +0800

    登录窗口

--- LoginForm.cs
+++ LoginForm.cs
@@ -1,4 +1,5 @@
 using System.Data.SqlClient;
+using System.Diagnostics;
 using static Library_management_system.Login_Sign_in;
 
 namespace Library_management_system {     public partial class LoginForm : Form     {         public LoginForm()         {             InitializeComponent();             this.StartPosition = FormStartPosition.CenterScreen;         }
@@ -20,15 +21,29 @@ namespace Library_management_system {     public partial class LoginForm : F
                 {
                     try
                     {
-                        using (SqlConnection con = new SqlConnection($"Data Source={DecryptString(GetJsonObject().DataSource)};Initial Catalog={DecryptString(GetJsonObject().InitialCatalog)};User ID={UserName.Text}; Password={MD5encipher(PassWord.Text)}"))
+                        SqlConnection con = new SqlConnection($"Data Source={DecryptString(GetJsonObject().DataSource)};Initial Catalog={DecryptString(GetJsonObject().InitialCatalog)};User ID={UserName.Text}; Password={MD5encipher(PassWord.Text)}");
+                        string sql = con.ConnectionString;
+                        con.Open();
+                        if (UserName.Text != "ROOT")
                         {
-                            con.Open();
                             UserForm.UserForm userForm = new UserForm.UserForm();
                             userForm.Show();
                             this.Hide();
                             userForm.FormClosing += (s, args) =>
                             {
                                 this.Show();
+                                con.Close();
+                            };
+                        }
+                        else
+                        {
+                            AdminForm.RootForm adminForm = new AdminForm.RootForm(sql);
+                            adminForm.Show();
+                            this.Hide();
+                            adminForm.FormClosing += (s, args) =>
+                            {
+                                this.Show();
+                                con.Close();
                             };
                         }
                     }
@@ -59,7 +74,7 @@ namespace Library_management_system {     public partial class LoginForm : F
                         using (SqlConnection con = new SqlConnection($"Data Source={DecryptString(GetJsonObject().DataSource)};Initial Catalog={DecryptString(GetJsonObject().InitialCatalog)};User ID={DecryptString(GetJsonObject().UserID)}; Password={DecryptString(GetJsonObject().Password)}"))
                         {
                             con.Open();
-                            string sql = $"create login {UserName.Text} with password=N'{MD5encipher(PassWord.Text)}';use [Library management system];create user {UserName.Text} for login {UserName.Text};grant select,update on [Book] to {UserName.Text};use master;alter login {UserName.Text} WITH DEFAULT_DATABASE = [Library management system];";
+                            string sql = $"create login {UserName.Text} with password=N'{MD5encipher(PassWord.Text)}';use [Library management system];create user {UserName.Text} for login {UserName.Text};grant select,update on [Book] to {UserName.Text};grant select,update on [User] to {UserName.Text};use master;alter login {UserName.Text} WITH DEFAULT_DATABASE = [Library management system];";
                             SqlCommand cmd = new SqlCommand(sql, con);
                             cmd.ExecuteNonQuery();
                         }
@@ -102,6 +117,55 @@ namespace Library_management_system {     public partial class LoginForm : F
             if (e.KeyCode == Keys.Enter)
             {
                 LoginButton_Click(sender, e);
+            }
+        }
+
+        private async void ChangePassWord_Click(object sender, EventArgs e)
+        {
+            if (UserName.Text != "" && PassWord.Text != "")
+            {
+                if (await Pingtest())
+                {
+                    try
+                    {
+                        using (SqlConnection con = new SqlConnection($"Data Source={DecryptString(GetJsonObject().DataSource)};Initial Catalog={DecryptString(GetJsonObject().InitialCatalog)};User ID={DecryptString(GetJsonObject().UserID)}; Password={DecryptString(GetJsonObject().Password)}"))
+                        {
+                            con.Open();
+                            string sql = $"ALTER LOGIN {UserName.Text} WITH PASSWORD = '{MD5encipher(PassWord.Text)}'";
+                            SqlCommand cmd = new SqlCommand(sql, con);
+                            cmd.ExecuteNonQuery();
+                        }
+                        MessageBox.Show("密码修改成功,即将将重启应用");
+                        Process process = new Process();
+                        process.Close();
+                        Application.Restart();
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show(ex.Message, "密码修改失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }                     catch (ArgumentNullException ex)
+                    {
+                        MessageBox.Show(ex.Message, "密码修改失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("无法连接至远程服务器", "连接错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                Label l = new Label();
+                l.Text = "请输入完整用户名或密码";
+                l.Location = new Point(255, 250);
+                l.AutoSize = true;
+                l.Name = "ErrorLabel";
+                l.ForeColor = Color.Red;
+                this.Controls.Add(l);
             }
         }
     } } 
