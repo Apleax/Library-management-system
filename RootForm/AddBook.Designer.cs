f4d391183b7da90b4b2cd73005fab4d97a083d2c
Apleax <3220923564@qq.com>
2024-06-20 01:22:05 +0800

    添加数据窗体

---
+++ RootForm/AddBook.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Library_management_system.RootForm
+{
+    partial class AddBook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            AddButton = new Button();
+            BooknameLabel = new Label();
+            AuthorLabel = new Label();
+            BooktypeLabel = new Label();
+            BooknumLabel = new Label();
+            Bookname = new TextBox();
+            Author = new TextBox();
+            Booktype = new TextBox();
+            Booknum = new TextBox();
+            Cancel = new Button();
+            SuspendLayout();
+            // 
+            // AddButton
+            // 
+            AddButton.Font = new Font("Microsoft YaHei UI", 14F);
+            AddButton.Location = new Point(99, 487);
+            AddButton.Name = "AddButton";
+            AddButton.Size = new Size(144, 53);
+            AddButton.TabIndex = 0;
+            AddButton.Text = "确定";
+            AddButton.UseVisualStyleBackColor = true;
+            AddButton.Click += AddButton_Click;
+            // 
+            // BooknameLabel
+            // 
+            BooknameLabel.AutoSize = true;
+            BooknameLabel.Font = new Font("Microsoft YaHei UI", 16F);
+            BooknameLabel.Location = new Point(99, 76);
+            BooknameLabel.Name = "BooknameLabel";
+            BooknameLabel.Size = new Size(82, 41);
+            BooknameLabel.TabIndex = 1;
+            BooknameLabel.Text = "书名";
+            // 
+            // AuthorLabel
+            // 
+            AuthorLabel.AutoSize = true;
+            AuthorLabel.Font = new Font("Microsoft YaHei UI", 16F);
+            AuthorLabel.Location = new Point(99, 164);
+            AuthorLabel.Name = "AuthorLabel";
+            AuthorLabel.Size = new Size(82, 41);
+            AuthorLabel.TabIndex = 2;
+            AuthorLabel.Text = "作者";
+            // 
+            // BooktypeLabel
+            // 
+            BooktypeLabel.AutoSize = true;
+            BooktypeLabel.Font = new Font("Microsoft YaHei UI", 16F);
+            BooktypeLabel.Location = new Point(99, 257);
+            BooktypeLabel.Name = "BooktypeLabel";
+            BooktypeLabel.Size = new Size(82, 41);
+            BooktypeLabel.TabIndex = 3;
+            BooktypeLabel.Text = "类型";
+            // 
+            // BooknumLabel
+            // 
+            BooknumLabel.AutoSize = true;
+            BooknumLabel.Font = new Font("Microsoft YaHei UI", 16F);
+            BooknumLabel.Location = new Point(99, 363);
+            BooknumLabel.Name = "BooknumLabel";
+            BooknumLabel.Size = new Size(82, 41);
+            BooknumLabel.TabIndex = 4;
+            BooknumLabel.Text = "库存";
+            // 
+            // Bookname
+            // 
+            Bookname.Font = new Font("Microsoft YaHei UI", 16F);
+            Bookname.Location = new Point(187, 73);
+            Bookname.Name = "Bookname";
+            Bookname.Size = new Size(348, 48);
+            Bookname.TabIndex = 7;
+            Bookname.TextChanged += Text_changed;
+            // 
+            // Author
+            // 
+            Author.Font = new Font("Microsoft YaHei UI", 16F);
+            Author.Location = new Point(187, 161);
+            Author.Name = "Author";
+            Author.Size = new Size(348, 48);
+            Author.TabIndex = 8;
+            Author.TextChanged += Text_changed;
+            // 
+            // Booktype
+            // 
+            Booktype.Font = new Font("Microsoft YaHei UI", 16F);
+            Booktype.Location = new Point(187, 254);
+            Booktype.Name = "Booktype";
+            Booktype.Size = new Size(348, 48);
+            Booktype.TabIndex = 9;
+            Booktype.TextChanged += Text_changed;
+            // 
+            // Booknum
+            // 
+            Booknum.Font = new Font("Microsoft YaHei UI", 16F);
+            Booknum.Location = new Point(187, 360);
+            Booknum.Name = "Booknum";
+            Booknum.Size = new Size(348, 48);
+            Booknum.TabIndex = 10;
+            Booknum.TextChanged += Text_changed;
+            // 
+            // Cancel
+            // 
+            Cancel.Font = new Font("Microsoft YaHei UI", 14F);
+            Cancel.Location = new Point(391, 487);
+            Cancel.Name = "Cancel";
+            Cancel.Size = new Size(144, 53);
+            Cancel.TabIndex = 11;
+            Cancel.Text = "取消";
+            Cancel.UseVisualStyleBackColor = true;
+            Cancel.Click += Cancel_Click;
+            // 
+            // AddBook
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(622, 629);
+            Controls.Add(Cancel);
+            Controls.Add(Booknum);
+            Controls.Add(Booktype);
+            Controls.Add(Author);
+            Controls.Add(Bookname);
+            Controls.Add(BooknumLabel);
+            Controls.Add(BooktypeLabel);
+            Controls.Add(AuthorLabel);
+            Controls.Add(BooknameLabel);
+            Controls.Add(AddButton);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "AddBook";
+            Text = "填写信息";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button AddButton;
+        private Label BooknameLabel;
+        private Label AuthorLabel;
+        private Label BooktypeLabel;
+        private Label BooknumLabel;
+        private TextBox Bookname;
+        private TextBox Author;
+        private TextBox Booktype;
+        private TextBox Booknum;
+        private Button Cancel;
+    }
+}
---
+++ RootForm/AddBook.cs
@@ -0,0 +1,99 @@
+﻿using System.Data.SqlClient;
+namespace Library_management_system.RootForm
+{
+    public partial class AddBook : Form
+    {
+        string sql;
+        public AddBook(string sql)
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.ControlBox = false;
+            this.sql = sql;
+        }
+        public string[] GetArray()
+        {
+            string[] array = new string[4];
+            array[0] = Bookname.Text;
+            array[1] = Author.Text;
+            array[2] = Booktype.Text;
+            array[3] = Booknum.Text;
+            return array;
+        }
+        private void Text_changed(object sender, EventArgs e)
+        {
+            if (Bookname.Text != "" || Author.Text != "" || Booktype.Text != "" || Booknum.Text != "")
+            {
+                foreach (Control c in this.Controls)
+                {
+                    if (c.Name == "ErrorLabel")
+                    {
+                        this.Controls.Remove(c);
+                    }
+                }
+            }
+        }
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            if (Bookname.Text != "" && Author.Text != "" && Booktype.Text != "" && Booknum.Text != "")
+            {
+                string[] book = GetArray();
+                try
+                {
+                    using (SqlConnection con = new SqlConnection(sql))
+                    {
+                        con.Open();
+                        SqlCommand cmd = new SqlCommand("USE [Library management system];INSERT INTO Book (Bookname, Author, Booktype, Booknum) VALUES (@Bookname, @Author, @Booktype, @Booknum)", con);
+                        cmd.Parameters.AddWithValue("@Bookname", book[0]);
+                        cmd.Parameters.AddWithValue("@Author", book[1]);
+                        cmd.Parameters.AddWithValue("@Booktype", book[2]);
+                        cmd.Parameters.AddWithValue("@Booknum", Int32.Parse(book[3]));
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                    MessageBox.Show("添加成功");
+                    this.Close();
+                }
+                catch (SqlException)
+                {
+                    using (SqlConnection con = new SqlConnection(sql))
+                    {
+                        con.Open();
+                        SqlCommand cmd = new SqlCommand($"USE [Library management system];SELECT * FROM Book WHERE Bookname = '{book[0]}' and Author='{book[1]}'", con);
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        reader.Read();
+                        string a = MessageBox.Show($"已经存在该书本:\n{reader.GetValue(0)}  {reader.GetValue(1)}  {reader.GetValue(2)}  {reader.GetValue(3)}\n是否改为增加库存", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question).ToString();
+                        reader.Close();
+                        if (a == "Yes")
+                        {
+                            SqlCommand cmd2 = new SqlCommand($"USE [Library management system];UPDATE Book SET Booknum = Booknum + {Int32.Parse(book[3])} WHERE Bookname = '{book[0]}' and Author='{book[1]}'", con);
+                            cmd2.ExecuteNonQuery();
+                            con.Close();
+                        }
+                        con.Close();
+                    }
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                Label l = new Label();
+                l.Text = "请填写完整信息";
+                l.Location = new Point(0, 0);
+                l.AutoSize = true;
+                l.Name = "ErrorLabel";
+                l.ForeColor = Color.Red;
+                l.Font = new Font("微软雅黑", 16);
+                this.Controls.Add(l);
+            }
+        }
+    }
+}
