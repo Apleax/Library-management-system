96c099eb8aa41d71371f894871c8969756e99041
Apleax <3220923564@qq.com>
2024-06-26 16:54:09 +0800

    注册窗口

---
+++ SigninForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Library_management_system
+{
+    partial class SigninForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Mailbox = new TextBox();
+            Mailboxlabel = new Label();
+            PassWord = new TextBox();
+            UserName = new TextBox();
+            Pass_Wordlabel = new Label();
+            User_Namelabel = new Label();
+            Sign_inButton = new Button();
+            SuspendLayout();
+            // 
+            // Mailbox
+            // 
+            Mailbox.Font = new Font("Microsoft YaHei UI", 14F);
+            Mailbox.Location = new Point(192, 53);
+            Mailbox.MaxLength = 20;
+            Mailbox.Name = "Mailbox";
+            Mailbox.Size = new Size(336, 43);
+            Mailbox.TabIndex = 1;
+            Mailbox.TextChanged += Text_changed;
+            // 
+            // Mailboxlabel
+            // 
+            Mailboxlabel.AutoSize = true;
+            Mailboxlabel.Font = new Font("Microsoft YaHei UI", 14F);
+            Mailboxlabel.Location = new Point(65, 53);
+            Mailboxlabel.Name = "Mailboxlabel";
+            Mailboxlabel.Size = new Size(95, 36);
+            Mailboxlabel.TabIndex = 14;
+            Mailboxlabel.Text = "邮   箱";
+            // 
+            // PassWord
+            // 
+            PassWord.Font = new Font("Microsoft YaHei UI", 14F);
+            PassWord.Location = new Point(192, 253);
+            PassWord.MaxLength = 16;
+            PassWord.Name = "PassWord";
+            PassWord.PasswordChar = '*';
+            PassWord.Size = new Size(336, 43);
+            PassWord.TabIndex = 3;
+            PassWord.TextChanged += Text_changed;
+            // 
+            // UserName
+            // 
+            UserName.Font = new Font("Microsoft YaHei UI", 14F);
+            UserName.Location = new Point(192, 154);
+            UserName.MaxLength = 20;
+            UserName.Name = "UserName";
+            UserName.Size = new Size(336, 43);
+            UserName.TabIndex = 2;
+            UserName.TextChanged += Text_changed;
+            // 
+            // Pass_Wordlabel
+            // 
+            Pass_Wordlabel.AutoSize = true;
+            Pass_Wordlabel.Font = new Font("Microsoft YaHei UI", 14F);
+            Pass_Wordlabel.Location = new Point(65, 256);
+            Pass_Wordlabel.Name = "Pass_Wordlabel";
+            Pass_Wordlabel.Size = new Size(95, 36);
+            Pass_Wordlabel.TabIndex = 11;
+            Pass_Wordlabel.Text = "密   码";
+            // 
+            // User_Namelabel
+            // 
+            User_Namelabel.AutoSize = true;
+            User_Namelabel.Font = new Font("Microsoft YaHei UI", 14F);
+            User_Namelabel.Location = new Point(65, 157);
+            User_Namelabel.Name = "User_Namelabel";
+            User_Namelabel.Size = new Size(99, 36);
+            User_Namelabel.TabIndex = 10;
+            User_Namelabel.Text = "用户名";
+            // 
+            // Sign_inButton
+            // 
+            Sign_inButton.Font = new Font("Microsoft YaHei UI", 14F);
+            Sign_inButton.Location = new Point(220, 352);
+            Sign_inButton.Name = "Sign_inButton";
+            Sign_inButton.Size = new Size(138, 52);
+            Sign_inButton.TabIndex = 16;
+            Sign_inButton.TabStop = false;
+            Sign_inButton.Text = "注册";
+            Sign_inButton.UseVisualStyleBackColor = true;
+            Sign_inButton.Click += Sign_inButton_Click;
+            // 
+            // SigninForm
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(592, 432);
+            Controls.Add(Sign_inButton);
+            Controls.Add(Mailbox);
+            Controls.Add(Mailboxlabel);
+            Controls.Add(PassWord);
+            Controls.Add(UserName);
+            Controls.Add(Pass_Wordlabel);
+            Controls.Add(User_Namelabel);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            KeyPreview = true;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "SigninForm";
+            Text = "SigninForm";
+            KeyUp += SigninForm_KeyUp;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox Mailbox;
+        private Label Mailboxlabel;
+        private TextBox PassWord;
+        private TextBox UserName;
+        private Label Pass_Wordlabel;
+        private Label User_Namelabel;
+        private Button Sign_inButton;
+    }
+}
---
+++ SigninForm.cs
@@ -0,0 +1,91 @@
+﻿using System.Data.SqlClient;
+using static Library_management_system.Login_Sign_in;
+
+namespace Library_management_system
+{
+    public partial class SigninForm : Form
+    {
+        public SigninForm()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+        private void Text_changed(object sender, EventArgs e)
+        {
+            if (UserName.Text != "" || PassWord.Text != "" || Mailbox.Text != "")
+            {
+                foreach (Control c in this.Controls)
+                {
+                    if (c.Name == "ErrorLabel")
+                    {
+                        this.Controls.Remove(c);
+                    }
+                }
+            }
+        }
+        private async void Sign_inButton_Click(object sender, EventArgs e)
+        {
+            if (UserName.Text != "" && PassWord.Text != "" && Mailbox.Text != "")
+            {
+                if (await Pingtest())
+                {
+                    try
+                    {
+                        if (MessageBox.Show($"邮箱:{Mailbox.Text}\n用户名:{UserName.Text}\n密码:{PassWord.Text}\n是否确认注册？", "注册确认", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            using (SqlConnection con = new SqlConnection($"Data Source={DecryptString(GetJsonObject().DataSource)};Initial Catalog={DecryptString(GetJsonObject().InitialCatalog)};User ID={DecryptString(GetJsonObject().UserID)}; Password={DecryptString(GetJsonObject().Password)}"))
+                            {
+                                con.Open();
+                                DateTime now = DateTime.Now;
+                                string sql2 = $"use [Library management system];INSERT INTO [User](nickname,mailbox,Username,[Last Login])VALUES('{UserName.Text}','{Mailbox.Text}','{UserName.Text}','{now}')";
+                                string sql = $"create login [{UserName.Text}] with password=N'{MD5encipher(PassWord.Text)}';use [Library management system];create user [{UserName.Text}] for login [{UserName.Text}];grant select,update on [Book] to [{UserName.Text}];grant select,update on [User] to [{UserName.Text}];use master;alter login [{UserName.Text}] WITH DEFAULT_DATABASE = [Library management system];";
+                                SqlCommand cmd = new SqlCommand(sql2, con);
+                                if (cmd.ExecuteNonQuery() == 1)
+                                {
+                                    cmd.CommandText = sql;
+                                    cmd.ExecuteNonQuery();
+                                    MessageBox.Show("注册成功");
+                                    this.Close();
+                                }
+                            }
+                        }
+                    }
+                    catch (SqlException)
+                    {
+                        MessageBox.Show("用户已存在", "注册失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    catch (ArgumentNullException ex)
+                    {
+                        MessageBox.Show(ex.Message, "注册失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("无法连接至远程服务器", "连接错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                Label l = new Label();
+                l.Text = "请输入完整用户名或密码和邮箱";
+                l.Location = new Point(255, 300);
+                l.AutoSize = true;
+                l.Name = "ErrorLabel";
+                l.ForeColor = Color.Red;
+                this.Controls.Add(l);
+            }
+        }
+
+        private void SigninForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Sign_inButton_Click(sender, e);
+            }
+        }
+    }
+}
